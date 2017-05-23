54fabc948f8b8b3dc7c20b42d0053e65ecc783b0
Ubuntu <ubuntu@ip-172-31-5-74.us-west-2.compute.internal>
2017-05-23 16:36:31 +0000

    REST calls implemented in fibonacci app

--- ch04/fibonacci/routes/fibonacci.js
+++ ch04/fibonacci/routes/fibonacci.js
@@ -4,19 +4,24 @@ var router = express.Router();
 var math = require('../math');
 router.get('/',function(req, res, next){
  if(req.query.fibonum){
-  math.fibonacciAsync(req.query.fibonum, (err, fiboval) => {
-   res.render('fibonacci', {
-    title: "Calculate Fibonacci numbers",
-    fibonum: req.query.fibonum,
-    fiboval: fiboval
-   })
+  var httpreq = require('http').request({
+   host: "localhost",
+   port: process.env.SERVERPORT,
+   path: "/fibonacci/"+Math.floor(req.query.fibonum),
+   method: 'GET'
+  }, 
+  httpresp => {
+   httpresp.on('data', chunk => {
+    var data = JSON.parse(chunk);
+    res.render('fibonacci',{
+     title: "Calculate Fibonacci numbers",
+     fibonum: req.query.fibonum,
+     fiboval: data.result
+    }); 
+   });
   });
-  // Calculate directly on this server
-  //res.render('fibonacci', {
-  // title: "Calculate Fibonacci numbers",
-  // fibonum: req.query.fibonum,
-  // fiboval: math.fibonacciLoop(req.query.fibonum)
-  //});
+  httpreq.on('error', err => { next(err); });
+  httpreq.end();
  } else {
   res.render('fibonacci', {
    title: "Calculate Fibonacci numbers",
@@ -24,5 +29,4 @@ router.get('/',function(req, res, next){
   });
  }
 });
-
 module.exports = router;
